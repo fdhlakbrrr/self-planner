c6e1d8462cd21c0529756f7f104d3809074540f4
m.fadhilakbar <muhammadfadhilakbar10@gmail.com>
2024-12-12 02:52:40 +0700

    test: push main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { HeadingText } from "@/components/molecule";
 // import { Text } from "@/components/ui/text";
 
 export default function Home() {
+  console.log("test")
   return (
     <Container customClass="p-0" align="CENTER">
       <Container
