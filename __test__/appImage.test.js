df4e0f2beaee005b924cecf046528b7515703312
m.fadhilakbar <muhammadfadhilakbar10@gmail.com>
2024-12-15 03:18:04 +0700

    test: run appImage snapshot testing

---
+++ __test__/appImage.test.js
@@ -0,0 +1,8 @@
+import { AppImage } from "../src/components/ui";
+import { render } from "@testing-library/react";
+
+it("renders correctly", () => {
+  const { container } = render(<AppImage name="money" />);
+
+  expect(container).toMatchSnapshot();
+});
